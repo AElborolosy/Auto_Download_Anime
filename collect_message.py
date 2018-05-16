a1320c2546c773357d266b3fdf2ab639f6cda455
AElborolosy <ahmed.elborolosy24@gmail.com>
2018-05-16 17:53:20 -0400

    Library Updates

--- collect_message.py
+++ collect_message.py
@@ -1,23 +1,35 @@
 """
-Connects to the xddc.horriblesubs.info site and queries the packlist for the pack number of the desired Anime.
+Connects to the xdcc.horriblesubs.info site and queries the packlist for the pack number of the desired Anime.
 """
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-def main():
+# Given the name of an anime (string), searches the pack list for it.
+def query_horrible_subs(anime):
 
     driver = webdriver.Chrome()
     driver.get('http://xdcc.horriblesubs.info')
+
     search_bar = driver.find_element_by_name('search')
-    search_bar.send_keys('Toji')
+    search_bar.send_keys(anime)
     search_bar.send_keys(Keys.ENTER)
 
-    episodes = driver.find_elements_by_class_name('anime0')
-    print(episodes)
+    # episodes = driver.find_elements_by_tag_name('tr')
+    episodes = driver.find_elements_by_id('name')
+    for episode in episodes:
+        print(episode.text)
 
+    driver.close()
     driver.quit()
+    return
+
+
+def main():
+
+    query_horrible_subs('Toji')
+
     return
 
 
