3c119f5d7adf3cbaeb773c6b862c0f7f4eeccc4f
AElborolosy <ahmed.elborolosy24@gmail.com>
2018-05-16 23:15:28 -0400

    Created 2nd Module

--- xdcc_send_request.py
+++ xdcc_send_request.py
@@ -6,6 +6,9 @@ Return: Downloaded Anime Series in Similar Named Folder.
 
 def main():
 
+    # For testing purposes, use this list of messages instead of generating a list with the collect_message module.
+    # Remove this list when the script combining the modules is completed.
+    temp_msg_list = ['/msg Ginpachi-Sensei xdcc send #3812', '/msg Ginpachi-Sensei xdcc send #3813']
     return
 
 
