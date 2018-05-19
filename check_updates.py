4316b89e17ec61d1cd3bc8e6b60c98a0fe48fe83
AElborolosy <ahmed.elborolosy24@gmail.com>
2018-05-19 18:02:04 -0400

    Added module that prints update messages to terminal for copy-pasting.

---
+++ check_updates.py
@@ -0,0 +1,25 @@
+"""
+Input: Text file, each line is a string containing the name of an anime
+Output: List of messages to send in Horriblesubs to download an update
+"""
+
+# Import collect message script.
+import collect_message as cm
+
+
+def main():
+    # Collect list of animes.
+    file_path = 'data\\anime_list.txt'
+    file = open(file_path, 'rU')
+    anime_list = [x for x in str(file.read()).split('\n')]
+    file.close()
+
+    # Print the updates for each anime.
+    for anime in anime_list:
+        episode_list = cm.main(anime)
+        print("%s: %s" %(anime,episode_list[-1]))
+    return
+
+
+if __name__ == '__main__':
+    main()
